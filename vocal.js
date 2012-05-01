07fb20af7548bed54d9a4b50fbfae34639327a14
Jason Douglas <Jason.Douglas@trimeego.com>
2012-05-01 05:45:23 -0700

    Minor updates

--- vocal.js
+++ vocal.js
@@ -17,12 +17,12 @@ app.post('/', function(req, res){
      
     var tropo = new TropoWebAPI();
      
-    var say = new Say("What's your destination?");
+    var say = new Say("Welcome to the Supplier Portal.  How can I help you?");
     var choices = new Choices("http://vocal.labs.icggroupinc.com/vocal.grxml");
  
     // (choices, attempts, bargein, minConfidence, name, recognizer, required, say, timeout, voice);
      
-    tropo.ask(choices, null, null, null, "destinations", null, null, say, 60, null);
+    tropo.ask(choices, null, null, null, "query", null, null, say, 60, null);
      
     tropo.on("continue", null, "/continue", true);
      
